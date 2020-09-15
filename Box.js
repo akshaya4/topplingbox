b9edc8802b444537dd7797365cf40e26a372d0c6
akshaya4 <70489515+akshaya4@users.noreply.github.com>
2020-09-15 15:25:30 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -2,18 +2,18 @@ class Box
 {
     //any new object when created - constructor - built in initializing function is invoked
     constructor(x,y,width,height)
-    {
-        var options = {
-            'restitution': 0.8,
-            'friction': 0.3,
-            'density': 1.0
-        }
-        this.body = Bodies.rectangle(x,y,width,height,options);
-        this.width = width;
-        this.height = height;
-         
-        World.add(world,this.body);
+
+    var options = {
+        'restitution':0.8,
+        'friction':0.3,
+        'density':1.0
     }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
 
     display()
     {
