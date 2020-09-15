26bb06da765441ce92af7e757158f00af1a76118
akshaya4 <70489515+akshaya4@users.noreply.github.com>
2020-09-15 14:53:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function setup()
     engine = Engine.create();
     world = engine.world;
     box1 = new Box(200,300,50,50);
-    box2 = new Box(240,100,50,50); 
+    box2 = new Box(240,100,50,100); 
    ground1 = new Ground(200,height,400,20)
 }
 
